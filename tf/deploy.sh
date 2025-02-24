c39f0b4e578dceec93311d1c7dc713e39bdbebff
lidorbashari <lidor1965@gmail.com>
2025-02-24 16:30:30 +0200

    test to save

---
+++ tf/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+apt-get update
+apt-get install ca-certificates curl -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# הוספת מקור Docker למאגרי Apt
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+usermod -aG docker ubuntu
+
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+curl -L "https://raw.githubusercontent.com/lidorbashari/NetflixInfra/main/docker-compose.yaml" -o /home/ubuntu/netflix-app/docker-compose.yml
+
+docker-compose up -d
--- tf/main.tf
+++ tf/main.tf
@@ -17,12 +17,62 @@ terraform {
 }
 
 provider "aws" {
-  region  = var.region
+  region = var.region
 }
 
+resource "aws_ebs_volume" "lidor_netflix_app_volume" {
+  availability_zone = "eu-north-1a"
+  size              = 5
+
+  tags = {
+    Name = "netflix_app_volume"
+  }
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "lidor-key-pair"
+  public_key = file("/home/lidorbashari/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "netflix_app_sg" {
+  name        = "lidor-netflix-app-sg" # change <your-name> accordingly
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_instance" "netflix_app" {
-  ami           = var.ami_id
-  instance_type = "t3.micro"
+  ami             = var.ami_id
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.netflix_app_sg.name]
+  key_name        = aws_key_pair.my_key.key_name
+  user_data = file("./deploy.sh")
+
+  ebs_block_device {
+    device_name           = "/dev/sdf"
+    volume_size           = 5
+    delete_on_termination = true
+  }
 
   tags = {
     Name      = "lidor-tf-netflix-${var.env}"
@@ -30,3 +80,7 @@ resource "aws_instance" "netflix_app" {
     Env       = var.env
   }
 }
+
+
+
+
